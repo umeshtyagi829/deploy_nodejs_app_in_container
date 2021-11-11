fafbe96796d42ee451757c29adfbc03367c28008
root <root@ip-172-31-21-23.ec2.internal>
2021-11-11 10:05:36 +0000

    second commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var app = express()
  
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello  World form Nodejs!')
+  res.send('Hello  World form Nodejs! "second commit"')
 })
  
  
