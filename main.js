0d6280fc562c832f74821822cdca83e7906f1ca8
root <root@ip-172-31-21-23.ec2.internal>
2021-11-11 07:56:56 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var express = require('express')    //Load express module with `require` directive
+var app = express() 
+ 
+//Define request response in root URL (/)
+app.get('/', function (req, res) {
+  res.send('Hello  World form Nodejs!')
+})
+ 
+ 
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
